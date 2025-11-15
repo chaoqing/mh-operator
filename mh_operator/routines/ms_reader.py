ff46b66533320b7b9f7284e23a46f4e14b15fda9
Nick Wang <chaoqingwang.nick@gmail.com>
2025-11-15 22:44:14 +0800

    efficent ms reader

--- mh_operator/routines/ms_reader.py
+++ mh_operator/routines/ms_reader.py
@@ -2,16 +2,31 @@ import os
 import struct
 from collections import Counter
 from functools import cached_property
+from itertools import islice
 
 import numpy as np
 
+from mh_operator.utils.common import logger
+
 
 class AgilentGCMSDataReader:
-    """Read the Agilent GCMS data.ms file descripped inside [Agilent .ms File Structure](https://github.com/evanyeyeye/rainbow/blob/ab0c6501901d2dbc746d96d99339f34be1f4c822/docs/source/agilent/ms.rst)"""
+    """Read the Agilent GCMS data.ms file described inside [Agilent .ms File Structure](https://github.com/evanyeyeye/rainbow/blob/ab0c6501901d2dbc746d96d99339f34be1f4c822/docs/source/agilent/ms.rst)"""
 
-    def __init__(self, file_object, prec=0):
+    def __init__(self, file_object):
+        FILE_TYPE_STR_OFFSET = 0x4  # File type string (GC / MS Data File)
+
+        # Validate file header.
+        file_object.seek(0)
+        (head_validation,) = struct.Struct(">I").unpack(file_object.read(4))
+        assert (
+            head_validation == 0x01320000
+        ), "Not correct magic number for Agilent GCMS data"
+
         self._f = file_object
-        self._prec = prec
+
+        # Determine the type of .ms file based on header.
+        type_ms_str = self._read_string(FILE_TYPE_STR_OFFSET, 1)
+        assert type_ms_str == "GC / MS Data File", "Only GC / MS Data File is supported"
 
     def _read_string(self, offset, gap=2):
         """
@@ -31,7 +46,8 @@ class AgilentGCMSDataReader:
         str_len = struct.unpack("<B", self._f.read(1))[0] * gap
         try:
             return self._f.read(str_len)[::gap].decode().strip()
-        except Exception:
+        except Exception as e:
+            logger.warning(f"Failed to read string from {offset}: {e}")
             return ""
 
     def _read_header(self, offsets, gap=2):
@@ -55,70 +71,62 @@ class AgilentGCMSDataReader:
 
     @cached_property
     def _raw_file_content_data(self):
-
         f = self._f
+
         short_unpack = struct.Struct(">H").unpack
         int_unpack = struct.Struct(">I").unpack
         little_short_unpack = struct.Struct("<H").unpack
 
-        data_offsets = {
-            "file_type_str": 0x4,  # File type string (GC / MS Data File)
-            "file_header_length_shorts": 0x10A,  # File header length (in shorts), Short, Big Endian
-            "gc_num_times": 0x142,  # Number of retention times (GC-MS), Short, Little Endian
-        }
-
-        # Validate file header.
-        f.seek(0)
-        head_validation = int_unpack(f.read(4))[0]
-        if head_validation != 0x01320000:
-            return None
-
-        # Determine the type of .ms file based on header.
-        type_ms_str = self._read_string(data_offsets["file_type_str"], 1)
-        if type_ms_str != "GC / MS Data File":
-            return None
-
         # Read the number of retention times from GC-MS specific offset.
-        f.seek(data_offsets["gc_num_times"])
-        num_times = little_short_unpack(f.read(2))[0]
+        SCAN_COUNT_OFFSET = (
+            0x142  # Number of retention times (GC-MS), Short, Little Endian
+        )
+        f.seek(SCAN_COUNT_OFFSET)
+        (num_times,) = little_short_unpack(f.read(2))
 
         # Go to the data start offset.
-        f.seek(data_offsets["file_header_length_shorts"])
-        file_header_length_shorts = short_unpack(f.read(2))[0]
+        FILE_HEADER_LENGTH_OFFSET = (
+            0x10A  # File header length (in shorts), Short, Big Endian
+        )
+        f.seek(FILE_HEADER_LENGTH_OFFSET)
+        (file_header_length_shorts,) = short_unpack(f.read(2))
         # The data body starts after the file header.
-        # The original code seeks to (file_header_length_shorts * 2 - 2)
         # This is to position the file pointer at the beginning of the first data segment.
         f.seek(file_header_length_shorts * 2 - 2)
 
         times_us = np.empty(num_times, dtype=np.uint32)
-        pair_counts = np.zeros(num_times, dtype=np.uint16)
-        pair_bytearr = bytearray()
+        scan_counts = np.zeros(num_times, dtype=np.uint16)
+        spectrum_pair_bytes = bytearray()
 
         for i in range(num_times):
             f.read(2)
             times_us[i] = int_unpack(f.read(4))[0]
             f.read(6)
-            pair_counts[i] = short_unpack(f.read(2))[0]
+            scan_counts[i] = short_unpack(f.read(2))[0]
             f.read(4)
 
-            pair_bytes = f.read(pair_counts[i] * 4)
-            pair_bytearr.extend(pair_bytes)
+            pair_bytes = f.read(scan_counts[i] * 4)
+            spectrum_pair_bytes.extend(pair_bytes)
 
             f.read(10)
 
-        return {
-            "times_us": times_us,
-            "pair_counts": pair_counts,
-            "pair_bytearr": pair_bytearr,
-            "num_times": num_times,
-        }
+        spectrum_pair_bytes = bytes(spectrum_pair_bytes)
+        spectrum_pair_counts = len(spectrum_pair_bytes) // 4
+
+        mzs_x20_int = np.ndarray(spectrum_pair_counts, ">H", spectrum_pair_bytes, 0, 4)
+        abundance_encoded = np.ndarray(
+            spectrum_pair_counts, ">H", spectrum_pair_bytes, 2, 4
+        )
+        abundance = np.multiply(
+            8 ** (abundance_encoded >> 14),
+            (abundance_encoded & 0x3FFF),
+            dtype=np.uint32,
+        )
+
+        return times_us, scan_counts, mzs_x20_int, abundance
 
     @cached_property
     def meta_data(self):
-        raw_content = self._raw_file_content_data
-        if raw_content is None:
-            return {}
-
         metadata_offsets = {
             "file_type": 0x4,  # File type
             "notebook": 0x18,  # Notebook name
@@ -145,97 +153,74 @@ class AgilentGCMSDataReader:
             gap=1,
         )
 
-        null_byte_metadata = self._read_header(
-            {
-                "unknown_1c0": metadata_offsets["unknown_1c0"],
-                "unknown_268": metadata_offsets["unknown_268"],
-                "method_466": metadata_offsets["method_466"],
-                "unknown_664": metadata_offsets["unknown_664"],
-                "unknown_862": metadata_offsets["unknown_862"],
-            },
-            gap=2,
+        metadata.update(
+            self._read_header(
+                {
+                    "unknown_1c0": metadata_offsets["unknown_1c0"],
+                    "unknown_268": metadata_offsets["unknown_268"],
+                    "method_466": metadata_offsets["method_466"],
+                    "unknown_664": metadata_offsets["unknown_664"],
+                    "unknown_862": metadata_offsets["unknown_862"],
+                },
+                gap=2,
+            )
         )
-        metadata.update(null_byte_metadata)
         return metadata
 
     @cached_property
-    def retention_time_us(self):
-        raw_content = self._raw_file_content_data
-        if raw_content is None:
-            return np.array([], dtype=np.uint32)
-        return raw_content["times_us"]
+    def retention_time(self):
+        times_us, *_ = self._raw_file_content_data
+        return times_us / 60000
 
     @cached_property
-    def raw_datas(self):
-        raw_content = self._raw_file_content_data
-        if raw_content is None:
-            return []
+    def mass_to_charge(self):
+        _, _, mzs_x20_int, _ = self._raw_file_content_data
+        return np.sort(np.unique(mzs_x20_int)) / 20
 
-        times_us = raw_content["times_us"]
-        pair_counts = raw_content["pair_counts"]
-        pair_bytearr = raw_content["pair_bytearr"]
-        num_times = raw_content["num_times"]
+    @property
+    def list_data(self):
+        times_us, scan_counts, mzs_x20_int, abundance = self._raw_file_content_data
 
-        raw_bytes = bytes(pair_bytearr)
-        total_paircount = np.sum(pair_counts)
+        spectrum_iter = zip(mzs_x20_int, abundance)
 
-        if total_paircount == 0:
-            return []
+        return [
+            (rt, list(islice(spectrum_iter, count)))
+            for rt, count in zip(times_us, scan_counts)
+        ]
 
-        mzs_raw_int = np.ndarray(total_paircount, ">H", raw_bytes, 0, 4)
-        int_encs = np.ndarray(total_paircount, ">H", raw_bytes, 2, 4)
-        int_heads = int_encs >> 14
-        int_tails = int_encs & 0x3FFF
-        int_values = np.multiply(8**int_heads, int_tails, dtype=np.uint32)
+    @property
+    def matrix_data(self):
+        times_us, scan_counts, mzs_x20_int, abundance = self._raw_file_content_data
+        unique_mzs = np.sort(np.unique(mzs_x20_int))
+        data = np.zeros((times_us.size, unique_mzs.size), dtype=np.uint32)
+        x_index = np.repeat(np.arange(times_us.size), scan_counts)
+        y_index = np.searchsorted(unique_mzs, mzs_x20_int)
+        data[x_index, y_index] = abundance
+        return data
 
-        raw_datas_list = []
-        cur_pair_index = np.uint32(0)  # Use numpy uint32 to prevent overflow warnings
-        for i in range(num_times):
-            current_rt_us = times_us[i]
-            current_pair_count = pair_counts[i]
-            current_spectrum_data = []
-            for j in range(current_pair_count):
-                # Ensure these are treated as numpy types to avoid scalar overflow warnings
-                mz_in_times_20_integer = mzs_raw_int[
-                    cur_pair_index + j
-                ].item()  # .item() converts to Python scalar
-                intensity = int_values[
-                    cur_pair_index + j
-                ].item()  # .item() converts to Python scalar
-                current_spectrum_data.append((mz_in_times_20_integer, intensity))
-            raw_datas_list.append(
-                (current_rt_us.item(), current_spectrum_data)
-            )  # .item() for rt_us as well
-            cur_pair_index += current_pair_count
-
-        return raw_datas_list
-
-    def get_real_spectrum(self, rt_minutes):
+    def spectrum_at(self, rt_minutes):
         """
         Returns the real spectrum [(real_mz_float_after_divide_20, intensity),...]
         for a given retention time (in minutes).
         """
-        times_us = self.retention_time_us
+        times_us, scan_counts, mzs_x20_int, abundance = self._raw_file_content_data
         target_rt_us = int(rt_minutes * 60000)
-
-        if times_us.size == 0:
-            return []
+        if (
+            times_us.size == 0
+            or target_rt_us < times_us[0] - 100
+            or target_rt_us > times_us[-1] + 100
+        ):
+            raise ValueError(
+                "Invalid retention time, should be in the recording range +-100ms"
+            )
 
         closest_rt_us_index = np.argmin(np.abs(times_us - target_rt_us))
-        closest_rt_us = times_us[closest_rt_us_index]
+        offset = np.sum(scan_counts[:closest_rt_us_index])
+        count = scan_counts[closest_rt_us_index]
 
-        spectrum_data = None
-        for rt_us, data_list in self.raw_datas:
-            if rt_us == closest_rt_us:
-                spectrum_data = data_list
-                break
+        spectrum = zip(
+            mzs_x20_int[offset : (offset + count)] / 20.0,
+            abundance[offset : (offset + count)],
+        )
 
-        if spectrum_data is None:
-            return []
-
-        real_spectrum = []
-        for mz_in_times_20_integer, intensity in spectrum_data:
-            real_mz_float = round(mz_in_times_20_integer / 20, self._prec)
-            real_spectrum.append((real_mz_float, intensity))
-
-        return real_spectrum
+        return list(spectrum)
